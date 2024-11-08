a7e2a6166badd9bbadb414a2dbb65258798c88dd
João Pedro Lima <joao.lima@izzyway.com.br>
2024-11-08 09:25:04 -0300

    feat: "Initial commit"

---
+++ src/debugging/Debugging.java
@@ -0,0 +1,13 @@
+package debugging;
+
+public class Debugging {
+    public static void main(String[] args) {
+        /*try {
+
+        } catch () {
+
+        } finally {
+
+        }*/
+    }
+}
