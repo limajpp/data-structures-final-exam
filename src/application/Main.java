8018b6c8c383dc548413d42bd7bcbbaaf75f3966
Gabriel Oliveira de Freitas <gabrieloftas@outlook.com>
2024-11-09 20:10:02 -0300

    feat:"implemented reading txt file"

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,21 @@
 package application;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String filePath = "/home/bl/IdeaProjects/data-structures-final-exam/src/txts/Input.txt";
+        try{
+            String content = Files.readString(Path.of(filePath),StandardCharsets.UTF_8);
+            System.out.print(content);
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+
+
+
     }
 }
