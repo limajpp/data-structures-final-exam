09ffe6e9da21f09fe87b660950a40c038be41df9
João Pedro Lima <joaopedroca90@gmail.com>
2024-11-09 20:43:06 -0300

    feat: "Implemented LinkedList most important methods and tested it on Debugging class"

--- src/debugging/Debugging.java
+++ src/debugging/Debugging.java
@@ -1,13 +1,40 @@
 package debugging;
 
+import entities.LinkedList;
+
 public class Debugging {
     public static void main(String[] args) {
-        /*try {
+        try {
+            LinkedList linkedList = new LinkedList();
 
-        } catch () {
+            System.out.println(linkedList);
 
+            linkedList.insertAtStart(1);
+            linkedList.insertAtStart(2);
+            linkedList.insertAtStart(3);
+            linkedList.insertAtStart(4);
+
+            System.out.println(linkedList);
+
+            linkedList.removeAtStart();
+            linkedList.removeAtStart();
+
+            System.out.println(linkedList);
+
+            linkedList.insertAtEnd(1);
+            linkedList.insertAtEnd(2);
+            linkedList.insertAtEnd(3);
+
+            System.out.println(linkedList);
+
+            linkedList.removeAtEnd();
+            linkedList.removeAtEnd();
+
+            System.out.println(linkedList);
+        } catch (Exception exception) {
+            exception.printStackTrace();
         } finally {
 
-        }*/
+        }
     }
 }
--- src/entities/LinkedList.java
+++ src/entities/LinkedList.java
@@ -2,6 +2,92 @@ package entities;
 
 public class LinkedList {
     private static class Node {
+        private final int ELEMENT;
+        private Node next;
 
+        private Node(int ELEMENT) {
+            this.ELEMENT = ELEMENT;
+            next = null;
+        }
+    }
+
+    private Node tail;
+    private int elements;
+
+    public LinkedList() {
+        tail = null;
+        elements = 0;
+    }
+
+    public boolean isEmpty() {
+        return elements == 0;
+    }
+
+    public void insertAtStart(int element) {
+        Node node = new Node(element);
+
+        if (isEmpty()) {
+            tail = node;
+        } else {
+            node.next = tail;
+            tail = node;
+        }
+
+        elements++;
+    }
+
+    public void removeAtStart() {
+        if (isEmpty()) throw new IllegalStateException("LinkedList is empty.");
+
+        tail = tail.next;
+        elements--;
+    }
+
+    public void insertAtEnd(int element) {
+        Node newNode = new Node(element);
+
+        if (isEmpty()) {
+            tail = newNode;
+        } else {
+            Node current = tail;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+
+        elements++;
+    }
+
+    public void removeAtEnd() {
+        if (isEmpty()) throw new IllegalStateException("LinkedList is empty.");
+
+        if (tail.next == null) {  // If there is only one element
+            tail = null;
+        } else {
+            Node current = tail;
+            while (current.next != null && current.next.next != null) {
+                current = current.next;
+            }
+            current.next = null;
+        }
+        elements--;
+    }
+
+    // Created for debugging...
+    @Override
+    public String toString() {
+        if (isEmpty()) return "Tail - null\nelements: " + elements + "\n";
+
+        StringBuilder linkedListString = new StringBuilder("Tail - ");
+        Node current = tail;
+
+        while (current != null) {
+            linkedListString.append(current.ELEMENT).append(" -> ");
+            current = current.next;
+        }
+
+        linkedListString.append("Head\n").append("elements: ").append(elements).append("\n");
+        return linkedListString.toString();
     }
 }
